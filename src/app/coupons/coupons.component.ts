b3027163f620bea402014f15c81b393e92ad26e7
punam-k <Punam@9396>
2020-03-27 10:53:13 +0530

    Second Commit with changes in dashboard and coupons page

--- src/app/coupons/coupons.component.ts
+++ src/app/coupons/coupons.component.ts
@@ -6,6 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coupons.component.css']
 })
 export class CouponsComponent implements OnInit {
+  coupons : Object=[
+    {
+      "image" : "../../assets/images/kids1.jpg",
+      "heading" : "Prime Deals",
+      "offer": "Kids Offer- Free Meals for Children ",
+      "detailsInfo" : "Min 25% off at the hottest premium restaurants",
+      
+    },
+    {
+      "image" : "../../assets/images/best-food.jpg",
+      "heading" : "50% OFF",
+      "offer": "Food At best offers on Orders | All users",
+      "detailsInfo" : "Mindblowing 50% off deals in your city",
+    },
+    {
+      "image" : "../../assets/images/drinks.jpg",
+      "heading" : "20% OFF on drinks",
+      "offer": "Flat 20% OFF On Orders | All users",
+      "detailsInfo" : "Enjoy flat 50% off on drinks and sweets",
+      
+    },
+    {
+      "image" : "../../assets/images/best-food.jpg",
+      "heading" : "50% OFF",
+      "offer": "Food At best offers on Orders | All users",
+      "detailsInfo" : "Mindblowing 50% off deals in your city",
+    },
+    {
+      "image" : "../../assets/images/drinks.jpg",
+      "heading" : "20% OFF on drinks",
+      "offer": "Flat 20% OFF On Orders | All users",
+      "detailsInfo" : "Enjoy flat 50% off on drinks and sweets",
+      
+    },
+    {
+      "image" : "../../assets/images/kids1.jpg",
+      "heading" : "Prime Deals",
+      "offer": "Kids Offer- Free Meals for Children ",
+      "detailsInfo" : "Minimum 25% off at the hottest and most premium restaurants",
+      
+    },
+ 
+  
+  ];
 
   
 
